f14328d436aa34606e94c1699ffb5c7252e171ae
Jakub Hoczek <hoczek.jakub@gmail.com>
2022-09-11 04:37:33 +0200

    Add controller logic

--- front/BBHack2k22.Front/Controllers/FilesaveController.cs
+++ front/BBHack2k22.Front/Controllers/FilesaveController.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.IO;
 using System.Net;
+using System.Net.Http.Headers;
 using System.Threading.Tasks;
 using BBHack2k22.Front.Models;
 using Microsoft.AspNetCore.Hosting;
@@ -15,24 +16,47 @@ public class FilesaveController : ControllerBase
 {
     private readonly IWebHostEnvironment env;
     private readonly ILogger<FilesaveController> logger;
+    private readonly IHttpClientFactory _clientFactory;
 
     public FilesaveController(IWebHostEnvironment env,
-        ILogger<FilesaveController> logger)
+        ILogger<FilesaveController> logger, IHttpClientFactory clientFactory)
     {
         this.env = env;
         this.logger = logger;
+        _clientFactory = clientFactory;
     }
 
     [HttpPost]
-    public async Task<IActionResult> PostFile(List<Byte[]> data)
+    public async Task<IActionResult> PostFile([FromForm] List<IFormFile> fileList)
     {
-
-        if (!Request.Headers.ContentType.Contains("multipart/form-data"))
-        {
-            return BadRequest("No multipart included");
-        }
+        long maxFileSize = 1024 * 1 * 1_000_000;
+        var imagesList = Request.Form.Files.Where(x => x.Name == "ImgFiles").ToList();
+        var translationList = Request.Form.Files.Where(x => x.Name == "TranslationFiles").ToList();
         
-        // var provider = new MultipartFormDataStreamProvider(root);
+        
+        using var content = new MultipartFormDataContent();
+        foreach (var file in imagesList)
+        {
+            var fileContent2 = new StreamContent(file.OpenReadStream());
+            fileContent2.Headers.ContentType = new MediaTypeHeaderValue(file.ContentType);
+
+            content.Add(content: fileContent2, name: "\"ImgFiles\"", fileName: file.Name);
+        }
+        foreach (var file in translationList)
+        {
+            var fileContent = new StreamContent(file.OpenReadStream());
+            fileContent.Headers.ContentType = new MediaTypeHeaderValue(file.ContentType);
+
+            content.Add(content: fileContent, name: "\"TranslationFiles\"", fileName: file.Name);
+        }
+
+        var client = _clientFactory.CreateClient();
+        
+        var response = await client.PostAsync("http://localhost:6000/" , content);
+        if (response.IsSuccessStatusCode)
+        {
+            
+        }
 
         return new CreatedResult("",new {});
     }
