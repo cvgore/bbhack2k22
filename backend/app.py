559bec20db52b5b768777edc5fe8c31fea2724f0
Kacper <kacper@cvgo.re>
2022-09-11 12:12:56 +0200

    pierdu pierdu

--- backend/app.py
+++ backend/app.py
@@ -1,30 +1,70 @@
-from flask import Flask, request
+import io
+import random
+import string
+import logging
+
+import cv2
+import matplotlib.pyplot as plt
+from zipfile import ZipFile
+from tempfile import TemporaryFile
+from flask import Flask, request, make_response, send_file
 from werkzeug.utils import secure_filename
+
 import font_metrics
 import translator
+from datetime import datetime
+import deleteTextInImage as dtii
+from ocr import Ocr
 
 app = Flask(__name__)
 tr = translator.Translator()
+ff = font_metrics.FontFitter()
+ocr = Ocr()
 
 
 @app.route("/", methods=['POST'])
 def hello_world():
-    images = request.files['images']
-    tr_data = request.files['translation_data']
+    images = request.files.getlist('images')
+    tr_data = request.files.getlist('translation')
+    timestamp = datetime.now().timestamp()
+    hash = ''.join(random.SystemRandom().choice(string.ascii_lowercase + string.hexdigits) for _ in range(8))
 
-    for file in images:
-        file.path = f"../var/uploads/{secure_filename(file.filename)}"
-        file.save(file.path)
+    images_to_process = []
+    translations_to_process = []
 
-    for file in tr_data:
-        file.path = f"../var/uploads/{secure_filename(file.filename)}"
-        file.save(file.path)
+    for i, file in enumerate(images):
+        path = f"../var/uploads/image_{timestamp}_{hash}_{i}.png"
+        images_to_process.append(path)
+        file.save(path)
+        logging.info(f'stored image at [{path}]')
 
-    tr.load_translation_file(tr_data[0].path)
-    ff = font_metrics.FontFitter()
-    ff.fit_text(..., tr.translate_text(...))
+    for i, file in enumerate(tr_data):
+        path = f"../var/uploads/translation_{timestamp}_{hash}_{i}.csv"
+        translations_to_process.append(path)
+        file.save(path)
+        logging.info(f'stored translation at [{path}]')
 
+    for tr_file in translations_to_process:
+        logging.info(f'loading translation [{tr_file}]')
+        tr.load_translation_file(tr_file)
 
-import deleteTextInImage as dtii
+        for img_file in images_to_process:
+            boxes, text_strs, img_ocr = ocr.get_text(img_file)
+            for box, text_str in zip(boxes, text_strs):
+                translated_text_str = tr.translate_text(text_str)
+                box, text_str, font_size = ff.fit_text(box, translated_text_str, 'sample.ttf')
 
-dtii.deleteTextInImage('zz.png')
+                img_ocr = dtii.deleteTextInImage(img_ocr, text_str, box)
+                cv2.imwrite(img_file, img_ocr)
+
+    tmpzip = io.BytesIO()
+    with ZipFile(tmpzip, mode='x') as zipfile:
+        for img_path in images_to_process:
+            zipfile.write(img_path, secure_filename(images[0].filename))
+
+        tmpzip.seek(0)
+
+        response = make_response(tmpzip.read())
+        response.headers.set('Content-Type', 'zip')
+        response.headers.set('Content-Disposition', 'attachment', filename='sample.zip')
+        return response
--- backend/deleteTextInImage.py
+++ backend/deleteTextInImage.py
@@ -21,35 +21,21 @@ def get_optimal_font_scale(text, width):
             return scale/10
     return 1
 
-def deleteTextInImage(image_list):
-    #foreach image in image_path
-    for image in image_list:
-        pipeline = keras_ocr.pipeline.Pipeline()
-        img_ocr = keras_ocr.tools.read(image)
-        prediction_groups = pipeline.recognize([img_ocr])
-        mask = np.zeros(img_ocr.shape[:2], dtype="uint8")
-        for prediction in prediction_groups[0]:
-            x0, y0 = prediction[1][0]
-            x1, y1 = prediction[1][1]
-            x2, y2 = prediction[1][2]
-            x3, y3 = prediction[1][3]
-            x_mid0, y_mid0 = midpoint(x1, y1, x2, y2)
-            x_mid1, y_mid1 = midpoint(x0, y0, x3, y3)
-            width = int(abs(x_mid0 - x_mid1))
-            thickness = int(math.sqrt((x2 - x1) ** 2 + (y2 - y1) ** 2))
-            cv2.line(mask, (x_mid0, y_mid0), (x_mid1, y_mid1), 255, thickness)
-            #here przeslac tekst do funkcji który zwrocilo tlumaczniee
-            font_scale = get_optimal_font_scale("eloo", width)
-            img_inpainted = cv2.inpaint(img_ocr, mask, 7, cv2.INPAINT_NS)
-            #here tez tekst i kolor i foncik
-            img_ocr = cv2.putText(img_inpainted, "eloo", (x_mid1, y_mid1), cv2.FONT_HERSHEY_SIMPLEX, font_scale, (255, 0, 0), 2)
-            #do nagrania demko to poniżej
-            #plt.imshow(img_ocr)
-            #plt.show()
-        img_rgb = cv2.cvtColor(img_ocr, cv2.COLOR_BGR2RGB)
-        cv2.imwrite(image, img_rgb)
+def deleteTextInImage(img_ocr, text_str, box):
+    mask = np.zeros(img_ocr.shape[:2], dtype="uint8")
+    x0, y0 = box[0]
+    x1, y1 = box[1]
+    x2, y2 = box[2]
+    x3, y3 = box[3]
+    x_mid0, y_mid0 = midpoint(x1, y1, x2, y2)
+    x_mid1, y_mid1 = midpoint(x0, y0, x3, y3)
+    width = int(abs(x_mid0 - x_mid1))
+    thickness = int(math.sqrt((x2 - x1) ** 2 + (y2 - y1) ** 2))
+    cv2.line(mask, (x_mid0, y_mid0), (x_mid1, y_mid1), 255, thickness)
+    font_scale = get_optimal_font_scale(text_str, width)
+    img_inpainted = cv2.inpaint(img_ocr, mask, 7, cv2.INPAINT_NS)
+    img_ocr = cv2.putText(img_inpainted, text_str, (x_mid1, y_mid1), cv2.FONT_HERSHEY_SIMPLEX, font_scale, (255, 0, 0), 2)
+    img_rgb = cv2.cvtColor(img_ocr, cv2.COLOR_BGR2RGB)
+    return img_rgb
 
-        zipObj = ZipFile('translated.zip', 'w')
-    for edited_image in image_list:
-        zipObj.write(edited_image)
-    zipObj.close()
+
